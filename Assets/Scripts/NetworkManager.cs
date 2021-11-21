d92f74fe5dfa6e3c9a0cd0def6f9dceca7c46a62
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-11-21 16:44:34 +0200

    Make simple client-server connection

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+
+        #if UNITY_EDITOR
+        Debug.Log("Build the project to start the server!");
+        #else
+        Server.Start(50, 26950);
+        #endif
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+
+class Server
+{
+    public static int MaxPlayers { get; private set; }
+    public static int Port { get; private set; }
+    private static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+    private static TcpListener tcpListener;
+
+    public static void Start(int _maxPlayers, int _port)
+    {
+        MaxPlayers = _maxPlayers;
+        Port = _port;
+
+            
+        Debug.Log("Starting server...");
+        InitializeServerData();
+
+        tcpListener = new TcpListener(IPAddress.Any, Port);
+        tcpListener.Start();
+        tcpListener.BeginAcceptTcpClient(TCPConnectCallback, null);
+
+        Debug.Log($"Server started on port {Port}.");
+    }
+
+    private static void TCPConnectCallback(IAsyncResult _result)
+    {
+        TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+        tcpListener.BeginAcceptTcpClient(TCPConnectCallback, null);
+        Debug.Log($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+        for (int i = 1; i <= MaxPlayers; i++)
+        {
+            if (clients[i].Tcp.socket == null)
+            {
+                clients[i].Tcp.Connect(_client);
+                return;
+            }
+        }
+
+        Debug.Log($"{_client.Client.RemoteEndPoint} failed to connect: Server full!");
+    }
+
+    private static void InitializeServerData()
+    {
+        for (int i = 1; i <= MaxPlayers; i++)
+        {
+            clients.Add(i, new Client(i));
+        }
+    }
+}
