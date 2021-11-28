ceddc5b7749ee6dca007c7cce3171dcdbd42683a
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-11-28 15:15:45 +0200

    Spawn a player at a random location on the field

--- Assets/Scripts/GameLogic/PlayersManager.cs
+++ Assets/Scripts/GameLogic/PlayersManager.cs
@@ -5,6 +5,10 @@ public class PlayersManager : MonoBehaviour
 {
     #region Fields
     [SerializeField] GameObject playerPrefab;
+    [Tooltip("The number that shows how many sectors you need to retreat " +
+        "from the edge of the field for players to spawn.")]
+    [SerializeField] int numberOfSectorsToIndentToSpawnPlayers;
+
     static PlayersManager instance;
     #endregion
 
@@ -22,17 +26,27 @@ public class PlayersManager : MonoBehaviour
         }
     }
 
-    public static Player SpawnPlayer(int id, string playerName)
+    static FieldSector GetFieldSectorForPlayerSpawn() => Field.GetSector(
+        Random.Range(instance.numberOfSectorsToIndentToSpawnPlayers,
+            Field.NumberOfSectorsPerHeight - instance.numberOfSectorsToIndentToSpawnPlayers),
+        Random.Range(instance.numberOfSectorsToIndentToSpawnPlayers,
+            Field.NumberOfSectorsPerWidth - instance.numberOfSectorsToIndentToSpawnPlayers));
+    static Vector2 GetRandomPositionInTheSector(Vector2 sectorSize,
+        Vector3 startFieldSectorPosition) => new Vector2(
+            startFieldSectorPosition.x + sectorSize.x* Random.value - 0.5f,
+            startFieldSectorPosition.y + sectorSize.y* Random.value - 0.5f);
+
+    public static Player SpawnPlayer(int id, string playerName) 
     {
         GameObject playerGameObject = PoolManager.GetGameObject(instance.playerPrefab.name);
 
-        // TODO: Spawn in random sector
-        FieldSector startFieldSector = Field.GetSector(0, 0);
-        // TODO: Spawn in random position in start sector
-        playerGameObject.transform.position = startFieldSector.transform.position; 
+        FieldSector startFieldSector = GetFieldSectorForPlayerSpawn();
+        playerGameObject.transform.position = GetRandomPositionInTheSector(Field.SectorSize,
+            startFieldSector.transform.position);
 
         Player player = playerGameObject.GetComponent<Player>();
         player.Controller.TargetPosition = playerGameObject.transform.position;
+
         player.Initialize(id, playerName);
         playerGameObject.SetActive(true);
 
